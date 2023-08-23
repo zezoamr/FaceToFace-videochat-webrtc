b44673588b41941bf679298df7c50377c239786b
ziad amr shalaby <zezoamr@yandex.com>
2023-08-23 22:32:41 +0300

    lobby + index styling + buttons + rooms

--- main.js
+++ main.js
@@ -2,11 +2,20 @@ let localStream
 let remoteStream
 let peerConnection
 
-let APP_ID = "d415e26a582c45a6b4f97620a719d031"
+let APP_ID = "d415e26a582c45a6b4f97620a719d031" //get from agora rtm web signaling, get js file from sdk
 let token = null
 let uid = String(Math.floor(Math.random() * 10000))
 let client
 let channel
+
+let queryString = window.location.search
+let urlParams = new URLSearchParams(queryString)
+let roomid = urlParams.get('room')
+
+if(!roomid) {
+    window.location = 'lobby.html'
+}
+
 
 const servers = {
     iceServers:[
@@ -103,20 +112,28 @@ let addAnswer = async (answer) => {
 }
 
 
+let constraints = {
+    video: {
+        width:{min:640, ideal: 1920, max:1920},
+        height:{min:480, ideal: 1080, max:1080}
+    },
+    audio: true
+}
+
 let init = async () => {
     try{
     client = await AgoraRTM.createInstance(APP_ID)
     await client.login({uid,token})
     
     //index.html/?roomid
-    channel = client.createChannel('main')
+    channel = client.createChannel (roomid) //('main')
     await channel.join()
 
     channel.on('MemberJoined', handleUserJoined)
-    channel.on('MemberLeft', handleUserLeft) //----
+    channel.on('MemberLeft', handleUserLeft) 
     client.on('MessageFromPeer', handleMessageFromPeer)
 
-    localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
+    localStream = await navigator.mediaDevices.getUserMedia(constraints)
     let videoElement = document.getElementById('user-1')
     videoElement.srcObject = localStream
     
@@ -127,6 +144,30 @@ let init = async () => {
     
 }
 
+let toggleCamera = async () => {
+    let videoTrack = localStream.getTracks().find(t => t.kind === 'video')
+    if(videoTrack.enabled){
+        videoTrack.enabled = false
+        document.getElementById('camera-btn').style.backgroundColor = 'rgb(255,80,80)'
+    }else{
+        videoTrack.enabled = true
+        document.getElementById('camera-btn').style.backgroundColor = 'rgb(179,102,249,.9)'
+    }
+}
+
+let toggleAudio = async () => {
+    let audioTrack = localStream.getTracks().find(t => t.kind === 'audio')
+    if(audioTrack.enabled){
+        audioTrack.enabled = false
+        document.getElementById('mic-btn').style.backgroundColor = 'rgb(255,80,80)'
+    }else{
+        audioTrack.enabled = true
+        document.getElementById('mic-btn').style.backgroundColor = 'rgb(179,102,249,.9)'
+    }
+}
+
+document.getElementById('camera-btn').addEventListener('click', toggleCamera)
+document.getElementById('mic-btn').addEventListener('click', toggleAudio)
 
 let leaveChannel = async () => {
     await channel.leave()
